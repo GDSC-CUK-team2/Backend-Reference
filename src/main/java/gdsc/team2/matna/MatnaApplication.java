873344794139066df0aa00c684bb6e7bd8fe1b25
HyunJe Park <sorsor999@naver.com>
2023-10-05 16:21:19 +0900

    feat : 초기 설정 및 DB 연동

---
+++ src/main/java/gdsc/team2/matna/MatnaApplication.java
@@ -0,0 +1,13 @@
+package gdsc.team2.matna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatnaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gdsc/team2/matna/MatnaApplicationTests.java
@@ -0,0 +1,13 @@
+package gdsc.team2.matna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatnaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
