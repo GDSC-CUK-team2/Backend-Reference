f847a55644b56cc9620a961183fc9d7c66f8b3d3
HyunJe Park <sorsor999@naver.com>
2023-10-29 01:55:55 +0900

    feat : accesstoken 구현

---
+++ src/main/java/gdsc/team2/matna/controller/AuthController.java
@@ -0,0 +1,43 @@
+package gdsc.team2.matna.controller;
+
+import gdsc.team2.matna.dto.JwtAuthDto;
+import gdsc.team2.matna.dto.LoginDto;
+import gdsc.team2.matna.dto.RegisterDto;
+import gdsc.team2.matna.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
+@Tag(name = "회원가입/로그인")
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    @Operation(summary = "로그인")
+    public JwtAuthDto login(@Valid @RequestBody LoginDto loginDto) {
+        log.info("로그인 stard");
+        String token = authService.login(loginDto);
+        log.info("로그인 end");
+        return new JwtAuthDto(token);
+    }
+
+    @PostMapping( "/signup")
+    @Operation(summary = "회원 가입")
+    public void register (@Valid @RequestBody RegisterDto registerDto) {
+        log.info("회원가입 start");
+        authService.register(registerDto);
+        log.info("회원가입 end");
+     }
+}
---
+++ src/main/java/gdsc/team2/matna/entity/Role.java
@@ -0,0 +1,8 @@
+package gdsc.team2.matna.entity;
+
+public enum Role {
+
+    ROLE_USER,
+    ROLE_ADMIN
+
+}
