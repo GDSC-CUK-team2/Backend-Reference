349b24bdd64bc9c219591b27f3555c0b05096f64
sor999 <sorsor999@naver.com>
2023-12-20 16:00:31 +0900

    feat : CORS 설정 수정  #4

--- src/main/java/gdsc/team2/matna/config/SecurityConfig.java
+++ src/main/java/gdsc/team2/matna/config/SecurityConfig.java
@@ -56,6 +56,8 @@ public class SecurityConfig {
 
                 .authorizeHttpRequests((authorize) ->
                         authorize
+
+                                .requestMatchers("").permitAll()
                                 .requestMatchers("/api/auth/**").permitAll()
                                 .requestMatchers("/api/map/**").permitAll()
                                 .requestMatchers("/api/restaurants/**").permitAll()
@@ -78,9 +80,14 @@ public class SecurityConfig {
         config.setAllowCredentials(true);
         config.addAllowedOrigin("http://localhost:3000");
         config.addAllowedOrigin("https://34.22.99.123");
+        config.addAllowedOrigin("https://35.216.62.134");
+        config.addAllowedOrigin("https://35.216.62.134:80");
+        config.addAllowedOrigin("https://35.216.62.134:3000");
+        config.addAllowedOrigin("https://35.216.62.134:8080");
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("*"));
+
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
